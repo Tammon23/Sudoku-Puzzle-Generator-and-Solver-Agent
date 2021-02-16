37aeea4f741d548feb3e1d6212e6a708c00ff9b7
Vlad Tusinean <58049695+tusineav@users.noreply.github.com>
2021-02-16 00:23:39 -0500

    Preliminary label work, commented code
    Comments are important

--- Sudoku.py
+++ Sudoku.py
@@ -4,15 +4,19 @@ from tkinter import Button, Label, Entry, Listbox, Canvas, BooleanVar, IntVar, D
 from tkinter.ttk import Notebook, Frame, Treeview, PanedWindow
 
 import numpy as np
-from math import floor
+from math import ceil, floor
 
+#Padding off the window. Wouldn't want the squares to render off the screen
 anchor_x = 10
 anchor_y = 10
 
-square_size = 50
+square_size = 55
 
+#The game board is a 9x9 grid of squares. Draw this grid on the canvas
 def create_game_board(canvas):
     i = anchor_x
+    
+    #Sudoku is always 9x9. No need to change this
     while i < 9 * square_size + anchor_x:    
         j = anchor_y
         while j < 9 * square_size + anchor_y:
@@ -28,7 +32,10 @@ def create_game_board(canvas):
 
     return game_board
 
+#Because of overlapping when filling a rectangle, the thicker lines need to be drawn constantly.
+#Thus, here's the function for it
 def draw_outer_lines(canvas):
+    #The thicker lines outline the 3x3 subgrids
     canvas.create_line(anchor_x + 3 * square_size, anchor_y, anchor_x + 3 * square_size, anchor_y + 9 * square_size, width = 3)
     canvas.create_line(anchor_x + 6 * square_size, anchor_y, anchor_x + 6 * square_size, anchor_y + 9 * square_size, width = 3)
 
@@ -60,14 +67,26 @@ def highlight_cross_section(canvas, highlights, row, column):
     highlight_row(canvas, highlights, column)
     highlight_column(canvas, highlights, row)
     
-    highlight_square(canvas, highlights, row, column, color = "#bae3e3", override = True)
+    #Highlight the central square a different color for pretty visualization
+    highlight_square(canvas, highlights, row, column, color = "#8de6e6", override = True)
 
+def highlight_sub_grid(canvas, highlights, row, column):
+    #There's a good chance we accidentally use this function for a square rather than a subgrid
+    if row > 2 or column > 2:
+        raise ValueError("Row/Column index too high. Perhaps you're using the wrong function?")
+    else:
+        for i in range(0, 3):
+            for j in range(0, 3):
+                highlight_square(canvas, highlights, i + 3 * row, j + 3 * column, override = True)
+
+#Set every highlight to False
 def reset_highlights(canvas, highlights):
     for i in range(0, 9):
         for j in range(0, 9):
             if highlights[i][j]:
-                highlight_square(canvas, highlights, i, j, False)
+                highlight_square(canvas, highlights, i, j, override = False)
 
+#Mathematically determine which square was clicked, then highlight the cross-section
 def right_click(event, canvas):
     square_x = floor((event.x - anchor_x) / square_size)
     square_y = floor((event.y - anchor_y) / square_size)
@@ -90,8 +109,36 @@ game_board = create_game_board(main_canvas)
 #We'll use this array to keep track of highlighted tiles
 highlights = np.zeros((9, 9))
 
-#highlight_square(main_canvas, highlights, 4, 2)
+#Testing code. Don't delete this guys, just comment it out until stuff is implemented
+highlight_sub_grid(main_canvas, highlights, 1, 0)
 
+
+temp = Label(root, text = "1   2   3\n4   5   6\n7   8   9", bg = "white")
+#temp.config(font = ("TkDefaultFont", ceil(square_size / 2)))
+temp.place(x = 0, y = 0)
+root.update()
+
+label_width = temp.winfo_width()
+label_height = temp.winfo_height()
+
+padding_x = (square_size - label_width) / 2
+padding_y = (square_size - label_height) / 2
+
+temp.place(x = anchor_x + padding_x + 2 * square_size, y = anchor_y + padding_y + 2 * square_size)
+
+temp = Label(root, text = "5", bg = "white")
+temp.config(font = ("TkDefaultFont", ceil(square_size / 2)))
+temp.place(x = 0, y = 0)
+root.update()
+
+label_width = temp.winfo_width()
+label_height = temp.winfo_height()
+
+padding_x = (square_size - label_width) / 2
+padding_y = (square_size - label_height) / 2
+
+temp.place(x = anchor_x + padding_x + 1 * square_size, y = anchor_y + padding_y + 2 * square_size)
+#Testing code ends here
 
 main_canvas.pack()
 
