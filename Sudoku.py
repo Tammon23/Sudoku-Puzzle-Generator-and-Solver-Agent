5ec754821f651b70123ce6b29010fa6ab5a6129f
Vlad Tusinean <58049695+tusineav@users.noreply.github.com>
2021-02-15 23:31:47 -0500

    Implemented cross-sectional coloring

--- Sudoku.py
+++ Sudoku.py
@@ -38,10 +38,10 @@ def draw_outer_lines(canvas):
     canvas.create_rectangle(anchor_x, anchor_y, anchor_x + 9 * square_size, anchor_y + 9 * square_size, width = 3)
         
 #If override is None, simply toggle the highlight. Otherwise, set it to override
-def highlight_square(canvas, highlights, square_x, square_y, override = None):
+def highlight_square(canvas, highlights, square_x, square_y, color = "#cfffff", override = None):
     highlights[square_x][square_y] = override if override is not None else not highlights[square_x][square_y]
 
-    square_fill = "#bae3e3" if highlights[square_x][square_y] == True else "white"
+    square_fill = color if highlights[square_x][square_y] == True else "white"
     canvas.create_rectangle(anchor_x + square_x * square_size, anchor_y + square_y * square_size, anchor_x + (square_x + 1) * square_size, anchor_y + (square_y + 1) * square_size, fill = square_fill , outline = "black")
 
     draw_outer_lines(canvas)
@@ -56,6 +56,12 @@ def highlight_column(canvas, highlights, column):
         #We don't want to toggle off already highlighted squares here on accident
         highlight_square(canvas, highlights, column, i, override = True)
 
+def highlight_cross_section(canvas, highlights, row, column):
+    highlight_row(canvas, highlights, column)
+    highlight_column(canvas, highlights, row)
+    
+    highlight_square(canvas, highlights, row, column, color = "#bae3e3", override = True)
+
 def reset_highlights(canvas, highlights):
     for i in range(0, 9):
         for j in range(0, 9):
@@ -68,8 +74,7 @@ def right_click(event, canvas):
     
     if 0 <= square_x <= 9 and 0 <= square_y <= 9:
         reset_highlights(canvas, highlights)
-        highlight_row(canvas, highlights, square_y)
-        highlight_column(canvas, highlights, square_x)
+        highlight_cross_section(canvas, highlights, square_x, square_y)
         
 
 root = tk.Tk()
@@ -85,9 +90,6 @@ game_board = create_game_board(main_canvas)
 #We'll use this array to keep track of highlighted tiles
 highlights = np.zeros((9, 9))
 
-highlight_row(main_canvas, highlights, 2)
-highlight_column(main_canvas, highlights, 2)
-reset_highlights(main_canvas, highlights)
 #highlight_square(main_canvas, highlights, 4, 2)
 
 
